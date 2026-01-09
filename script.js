f7a4c239aef1b252979fad249f3882ff403a178d
Ivan <68528490+Inara5@users.noreply.github.com>
2026-01-09 16:52:11 -0600

    Add backspace and keyboard support

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let seven = document.getElementById("seven")
 let eight = document.getElementById("eight")
 let nine = document.getElementById("nine")
 let zero = document.getElementById("zero")
+let decimal = document.getElementById("decimal")
 let plus = document.getElementById("plus")
 let minus = document.getElementById("minus")
 let times = document.getElementById("times")
 let divide = document.getElementById("divide")
 let equals = document.getElementById("equals")
+let backspace = document.getElementById("backspace")
 let clear = document.getElementById("clear")
 
 let display = document.getElementById("display")
@@ -24,7 +26,146 @@ let secondNumber = 0
 let done = 0
 let counter = 0
 
-//one.addEventListener("click", e => display.textContent += e.target.textContent)
+document.addEventListener("keydown", (e) => {
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    numbers.filter(num => {
+        if (e.key == num) {
+            if (done > 0) {
+        display.textContent = ""
+    }
+    done = 0
+    display.textContent += e.key
+        }
+    })
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Backspace") {
+        if (done > 0) {
+        display.textContent = ""
+    }
+    display.textContent = display.textContent.slice(0, -1)
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "+") {
+        counter += 1
+    console.log(counter)
+    if (display.textContent == "" && operator == "" || done !== 0 && operator !== "" || display.textContent == "Not Possible !") {
+        alert("Please choose a number first !")
+        counter -= 1
+    } else if (counter > 1) {
+        secondNumber = Number(display.textContent)
+        let answer = operate(operator, firstNumber, secondNumber)
+        display.textContent = answer
+        firstNumber = answer
+        operator = "plus"
+        done = 1
+    } else {
+        firstNumber = Number(display.textContent)
+        operator = "plus"
+        display.textContent = ""
+    }
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "-") {
+        counter += 1
+    console.log(counter)
+    if (display.textContent == "" && operator == "" || done !== 0 && operator !== "" || display.textContent == "Not Possible !") {
+        alert("Please choose a number first !")
+        counter -= 1
+    } else if (counter > 1) {
+        secondNumber = Number(display.textContent)
+        let answer = operate(operator, firstNumber, secondNumber)
+        display.textContent = answer
+        firstNumber = answer
+        operator = "minus"
+        done = 1
+    } else {
+        firstNumber = Number(display.textContent)
+        operator = "minus"
+        display.textContent = ""
+    }
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "*") {
+        counter += 1
+    if (display.textContent == "" && operator == "" || done !== 0 && operator !== "" || display.textContent == "Not Possible !") {
+        alert("Please choose a number first !")
+        counter -= 1
+    } else if (counter > 1) {
+        secondNumber = Number(display.textContent)
+        let answer = operate(operator, firstNumber, secondNumber)
+        display.textContent = answer
+        firstNumber = answer
+        operator = "times"
+        done = 1
+    } else {
+        firstNumber = Number(display.textContent)
+        operator = "times"
+        display.textContent = ""
+    }
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "/") {
+        counter += 1
+    if (display.textContent == "" && operator == "" || done !== 0 && operator !== "" || display.textContent == "Not Possible !") {
+        alert("Please choose a number first !")
+        counter -= 1
+    } else if (counter > 1) {
+        secondNumber = Number(display.textContent)
+        let answer = operate(operator, firstNumber, secondNumber)
+        display.textContent = answer
+        firstNumber = answer
+        operator = "divide"
+        done = 1
+    } else {
+        firstNumber = Number(display.textContent)
+        operator = "divide"
+        display.textContent = ""
+    }
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        secondNumber = Number(display.textContent)
+    if (firstNumber == 0 && secondNumber == 0 && operator == "divide") {
+        display.textContent = "Not Possible !"
+        firstNumber = 0
+        operator = ""
+        secondNumber = 0
+        counter = 0
+        done = 1
+    } else if (display.textContent == "" && done == 0 || firstNumber == display.textContent && done == 1) {
+        alert("Please choose a number first !")
+    } else if (operator == "" && done == 1){
+        alert("Please choose a number or operator first !")
+    } else if (display.textContent !== "" && operator == "") {
+        alert("Please choose an operator first !")
+    } else {
+        let answer = operate(operator, firstNumber, secondNumber)
+        display.textContent = answer
+        done = 1
+        counter = 0
+        firstNumber = 0
+        operator = ""
+        secondNumber = 0
+    }
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key == "c") {
+        display.textContent = ""
+        done = 0
+        firstNumber = 0
+        operator = ""
+        secondNumber = 0
+        counter = 0
+    }
+})
+
 one.addEventListener("click", (e) => {
     if (done > 0) {
         display.textContent = ""
@@ -94,6 +235,20 @@ zero.addEventListener("click", (e) => {
     }
     done = 0
     display.textContent += e.target.textContent
+})
+/*decimal.addEventListener("click", (e) => {
+    if (done > 0) {
+        display.textContent = ""
+    }
+    done = 0
+    display.textContent += e.target.textContent
+}, {once:true}
+)*/
+backspace.addEventListener("click", () => {
+    if (done > 0) {
+        display.textContent = ""
+    }
+    display.textContent = display.textContent.slice(0, -1)
 })
 
 plus.addEventListener("click", () => {
@@ -195,7 +350,6 @@ equals.addEventListener("click", () => {
         operator = ""
         secondNumber = 0
     }
-    
 })
 
 clear.addEventListener("click", () => {
